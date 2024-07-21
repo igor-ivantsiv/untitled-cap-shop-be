f8133048f22c1bc785d0abb5e7ef1ec995516d7a
Thomas Kruithof <tkruithof95@gmail.com>
2024-07-21 14:34:03 +0200

    few more auth tests

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,9 +1,13 @@
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const router = require("express").Router();
+const mongoose = require("mongoose");
 
 const User = require("../models/User.model");
-const { isAuthenticated, authorizeRole } = require("../middlewares/route-guard.middleware");
+const {
+  isAuthenticated,
+  authorizeRole,
+} = require("../middlewares/route-guard.middleware");
 
 // register
 router.post("/register", async (req, res, next) => {
@@ -19,6 +23,9 @@ router.post("/register", async (req, res, next) => {
 
     res.status(201).json(newUser);
   } catch (error) {
+    if (error.code === 11000) {
+      res.status(409).json({ message: "Username already taken" });
+    }
     next(error);
   }
 });
@@ -57,12 +64,17 @@ router.get("/verify", isAuthenticated, (req, res, next) => {
   //res.status(200).json({message: "token validated"})
 });
 
-router.get("/verify/admin", isAuthenticated, authorizeRole, (req, res, next) => {
+router.get(
+  "/verify/admin",
+  isAuthenticated,
+  authorizeRole,
+  (req, res, next) => {
     //res.status(200).json(req.tokenPayload);
-    res.status(200).json({message: "admin token validated"})
-})
+    res.status(200).json({ message: "admin token validated" });
+  }
+);
 
-// get all users (test)
+// (TEST) get all users
 router.get("/", async (req, res, next) => {
   try {
     const users = await User.find();
@@ -80,7 +92,31 @@ router.get("/users", isAuthenticated, authorizeRole, async (req, res, next) => {
   } catch (error) {
     next(error);
   }
-})
+});
 
+// (TEST) get one user to display profile
+router.get("/users/:userId", isAuthenticated, async (req, res, next) => {
+  const { userId } = req.params;
+
+  if (!mongoose.isValidObjectId(userId)) {
+    return next(new Error("invalid ID"));
+  }
+
+  if (userId !== req.tokenPayload.userId) {
+    res.status(403).json({ message: "Forbidden" });
+    return next(new Error("User not authorized"));
+  }
+
+  try {
+    const userProfile = await User.findById(userId);
+    if (!userProfile) {
+      res.status(404).json({ message: "User profile not found" });
+      return next(new Error("User not found"));
+    }
+    res.status(200).json(userProfile);
+  } catch (error) {
+    next(error);
+  }
+});
 
 module.exports = router;
