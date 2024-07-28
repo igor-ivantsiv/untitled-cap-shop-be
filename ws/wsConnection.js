a91a447f2cb747749ab1655ca77864b90c1ebc8e
Thomas Kruithof <tkruithof95@gmail.com>
2024-07-28 20:16:07 +0200

    working on chat

--- ws/wsConnection.js
+++ ws/wsConnection.js
@@ -1,29 +1,54 @@
 const expressWs = require("express-ws");
 const { clearCartAndUpdateStock } = require("./dbOperations");
+const Message = require("../models/Message.model");
+const User = require("../models/User.model");
 
 const logWsError = (err, context) => {
   console.error("ERROR IN WS: ", context, err);
 };
 
+const findUsername = async (id) => {
+  let username = "Not-found";
+  try {
+    const foundUsername = await User.findById(id).select("username");
+    username = foundUsername.username
+  } catch (error) {
+    logWsError("error finding username");
+  }
+  return username;
+};
 
+const handleMessage = async (senderId, message, connectedUsers, username) => {
+  const { recipientId, content } = message;
+
+  try {
+    const newMessage = await Message.create({
+      senderId: senderId,
+      recipientId: recipientId,
+      content,
+    });
+
+    if (!newMessage) {
+      logWsError(error, "WebSocket error -creating message in db");
+    }
+
+    const recipientWs = connectedUsers.get(recipientId);
+    console.log("WS RECIPIENT: ", recipientId);
+    if (recipientWs) {
+      console.log("WS MESSAGE SENT");
+      recipientWs.send(JSON.stringify({ senderId, username, content }));
+    }
+  } catch (error) {
+    logWsError(error, "WebSocket error -handle message");
+  }
+};
 
 const connectWs = (app) => {
   expressWs(app);
 
-  const connectedUsers = new Map()
+  const connectedUsers = new Map();
 
-  const handleMessage = (senderId, message) => {
-    const {recipientId, content} = message;
-  
-    const recipientWs = connectedUsers.get(recipientId)
-    console.log("WS RECIPIENT: ", recipientId)
-    if (recipientWs) {
-      console.log("WS MESSAGE SENT")
-      recipientWs.send(JSON.stringify({ senderId, content }))
-    }
-  }
-
-  app.ws("/ws", (ws, req) => {
+  app.ws("/ws", async (ws, req) => {
     const userId = req.query.userId;
 
     if (!userId) {
@@ -31,23 +56,53 @@ const connectWs = (app) => {
       return;
     }
 
+    const username = await findUsername(userId);
+
     console.log("WS CONNECTED: ", userId);
-    connectedUsers.set(userId, ws)
+    console.log("USERNAME: ", username);
+    connectedUsers.set(userId, ws);
+
+    try {
+      // retrieve unread messages
+      const unreadMessages = await Message.find({
+        recipientId: userId,
+        read: false,
+      }).populate("senderId", "username");
+
+      // send unread messages
+      unreadMessages.forEach((msg) => {
+        ws.send(
+          JSON.stringify({
+            senderId: msg.senderId._id,
+            username: msg.senderId.username,
+            content: msg.content,
+            timestamp: msg.createdAt,
+          })
+        );
+      });
+
+      // mark messages as read
+      await Message.updateMany(
+        { recipientId: userId, read: false },
+        { read: true }
+      );
+    } catch (error) {
+      logWsError(error, "WebSocket error -unread messages");
+    }
 
     ws.on("message", (message) => {
       console.log("WS MESSAGE: ", message);
-      const parsedMessage = JSON.parse(message)
-      handleMessage(userId, parsedMessage)
+      const parsedMessage = JSON.parse(message);
+      handleMessage(userId, parsedMessage, connectedUsers, username);
     });
 
     ws.on("close", async () => {
       console.log("WS USER DISCONNECTED: ", userId);
-      connectedUsers.delete(userId)
+      connectedUsers.delete(userId);
       try {
-        await clearCartAndUpdateStock(userId)
-      }
-      catch (error) {
-        logWsError(error, "WebSocket Error on disconnection")
+        await clearCartAndUpdateStock(userId);
+      } catch (error) {
+        logWsError(error, "WebSocket Error on disconnection");
       }
     });
 
