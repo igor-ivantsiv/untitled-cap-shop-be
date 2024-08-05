d37188d7275b68d0a2034b8a3a7abd227c4e67cc
Thomas Kruithof <tkruithof95@gmail.com>
2024-08-05 18:00:16 +0200

    removed testing routes from auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -78,49 +78,5 @@ router.get(
   }
 );
 
-// (TEST) get all users
-router.get("/", async (req, res, next) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// (TEST) verify a path that only admin can reach
-router.get("/users", isAuthenticated, authorizeRole, async (req, res, next) => {
-  try {
-    const users = await User.find();
-    res.status(200).json(users);
-  } catch (error) {
-    next(error);
-  }
-});
-
-// (TEST) get one user to display profile
-router.get("/users/:userId", isAuthenticated, async (req, res, next) => {
-  const { userId } = req.params;
-
-  if (!mongoose.isValidObjectId(userId)) {
-    return next(new Error("invalid ID"));
-  }
-
-  if (userId !== req.tokenPayload.userId) {
-    res.status(403).json({ message: "Forbidden" });
-    return next(new Error("User not authorized"));
-  }
-
-  try {
-    const userProfile = await User.findById(userId);
-    if (!userProfile) {
-      res.status(404).json({ message: "User profile not found" });
-      return next(new Error("User not found"));
-    }
-    res.status(200).json(userProfile);
-  } catch (error) {
-    next(error);
-  }
-});
 
 module.exports = router;
